0d66716e0511b8d5252dd91d9ff3c16e019a484f
nozomemein <b8yukifsukeo999n@gmail.com>
2025-11-09 10:38:12 +0900

    refactor utils

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use core::panic;
 use std::env;
-use std::ffi::OsString;
-use std::fs::File;
+
 use std::io::BufWriter;
 use std::io::Read;
 use std::io::Write;
-use std::path::Path;
 
 mod analyzer;
 mod generator;
 mod lexer;
 mod parser;
+mod utils;
 
 use generator::Generator;
 
@@ -23,7 +22,8 @@ fn main() -> Result<(), std::io::Error> {
         panic!("Please provide a file name as an argument.");
     }
 
-    let (mut input_file, output_file) = get_file_name(&args).expect("Failed to get file name");
+    let (mut input_file, output_file) =
+        utils::get_file_name(&args).expect("Failed to get file name");
 
     let mut input = String::new();
 
@@ -45,26 +45,4 @@ fn main() -> Result<(), std::io::Error> {
     buf_writer.flush()?;
 
     Ok(())
-}
-
-fn get_file_name(args: &[String]) -> Result<(File, File), std::io::Error> {
-    if args.len() < 2 {
-        panic!("Please provide a file name as an argument.");
-    }
-
-    let input_file_path = Path::new(&args[1]);
-    let input_file = File::open(input_file_path).expect("Failed to open the file");
-
-    let mut buffer = OsString::with_capacity(input_file_path.as_os_str().len());
-    buffer.push(
-        input_file_path
-            .file_stem()
-            .expect("Failed to get file stem"),
-    );
-    buffer.push(".s");
-
-    let output_file_path = Path::new(buffer.as_os_str());
-    let output_file = File::create(output_file_path).expect("Failed to create the file");
-
-    Ok((input_file, output_file))
 }
